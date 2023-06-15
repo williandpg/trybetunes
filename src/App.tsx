08d89f7010e752f9858fa4559d859390b9c9428c
Willian Gonçalves <willian.p.g@outlook.com>
2023-06-15 18:39:11 -0300

    Requisito 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+
 function App() {
   return (
-    <p>Trybetunes</p>
+    <>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+      </Routes>
+      ,
+    </>
   );
 }
 
---
+++ src/pages/Login.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { createUser } from '../services/userAPI';
+
+function Login() {
+  const [nameLogin, setNameLogin] = useState('');
+  const [load, setLoad] = useState(false);
+
+  const handleSubmit = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setNameLogin(value);
+  };
+
+  const handleLoading = async () => {
+    setLoad(true);
+    await createUser({ name: nameLogin });
+    setLoad(false);
+  };
+
+  if (load) return <p>Carregando...</p>;
+
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          id="name"
+          data-testid="login-name-input"
+          value={ nameLogin }
+          onChange={ handleSubmit }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-button"
+        onClick={ handleLoading }
+        disabled={ nameLogin.length < 3 }
+      >
+        Entrar
+      </button>
+    </form>
+  );
+}
+
+export default Login;
