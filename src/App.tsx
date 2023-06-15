c08a83645b464e18603347f35dd270a792c0d2fc
Willian Gonçalves <willian.p.g@outlook.com>
2023-06-15 19:07:18 -0300

    Iniciando o requisito 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Search from './pages/Search';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={ <Login /> } />
+        <Route path="/search" element={ <Search /> } />
       </Routes>
       ,
     </>
