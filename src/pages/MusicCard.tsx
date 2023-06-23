e9f4a19089c7bdd1b98d256b2f7a70c53026d5b0
Willian Gonçalves <willian.p.g@outlook.com>
2023-06-22 21:20:50 -0300

    Requisito 05

--- src/pages/MusicCard.tsx
+++ src/pages/MusicCard.tsx
@@ -1,4 +1,14 @@
+import React, { useState } from 'react';
+
 function MusicCard({ song }: { song: any[] }) {
+  const [favoriteMusic, setFavoriteMusic] = useState<boolean>(false);
+
+  const handleFavoriteMusic = (event:
+  { target: { checked: boolean |
+  ((prevState: boolean) => boolean); }; }) => {
+    setFavoriteMusic(event.target.checked);
+  };
+
   return (
     <ul>
       {song.map((index) => {
@@ -6,12 +16,26 @@ function MusicCard({ song }: { song: any[] }) {
           <li key={ index.trackId }>
             <p>{index.trackName}</p>
             <section>
-              <audio data-testid="audio-component" src="{previewUrl}" controls>
+              <audio data-testid="audio-component" src={ index.previewUrl } controls>
                 <track kind="captions" />
                 O seu navegador não suporta o elemento
                 {' '}
                 <code>audio</code>
               </audio>
+              <label
+                htmlFor="favorite"
+                data-testid={ `checkbox-music-${index.trackId}` }
+              >
+                { favoriteMusic
+                  ? <img src="/src/images/checked_heart.png" alt="favorite" />
+                  : <img src="/src/images/empty_heart.png" alt="favorite" /> }
+              </label>
+              <input
+                type="checkbox"
+                id="favorite"
+                name="favorite"
+                onChange={ handleFavoriteMusic }
+              />
             </section>
           </li>
         );
